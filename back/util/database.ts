abde97b50b14ff29a70285aebf2acb0c4fc6efbe
wjdtj9656 <wjdtj9656@naver.com>
2023-01-29 01:08:48 +0900

    style: change host

--- back/util/database.ts
+++ back/util/database.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
-// const sequelize = new Sequelize("cypp", "cyppmaster", process.env.DB_PASSWORD, {
-//   host: "cyppdb.chvb05f017rb.ap-northeast-2.rds.amazonaws.com",
-const sequelize = new Sequelize("exuser", "root", process.env.DB_PASSWORD, {
-  host: "localhost",
+const sequelize = new Sequelize("cypp", "cyppmaster", process.env.DB_PASSWORD, {
+  host: "cyppdb.chvb05f017rb.ap-northeast-2.rds.amazonaws.com",
+  // const sequelize = new Sequelize("exuser", "root", process.env.DB_PASSWORD, {
+  //   host: "localhost",
   dialect: "mysql",
   port: 3306,
   define: {
