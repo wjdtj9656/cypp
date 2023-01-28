dd146f5f61756506ba5da4806e218220e1ce544f
wjdtj9656 <wjdtj9656@naver.com>
2023-01-28 23:32:34 +0900

    style: divide local and server

--- front/src/Api/api.tsx
+++ front/src/Api/api.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 // let URL = "http://10.0.2.2:8080/api";
 let URL = "http://cypp.link:8000";
+// let URL = "http://localhost:8000";
 // if (window.location.hostname === "localhost") {
 //   URL = `http://localhost:8080/api`;
 // } else {
