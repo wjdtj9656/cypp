b84113548d2d07f77850fb155015ce4d7e6238f6
wjdtj9656 <wjdtj9656@naver.com>
2023-02-24 15:52:22 +0900

    STYLE: http to https

--- back/index.ts
+++ back/index.ts
@@ -8,6 +8,8 @@ import { saveUserNickname } from "./controllers/users";
 import bodyParser from "body-parser";
 import { loadComment, saveComments } from "./controllers/comments";
 import { router } from "./routes/user";
+import https from "https";
+import fs from "fs";
 const app = express();
 const allowedOrigins = ["http://localhost:3000", "http:cypp.link:3000", "http:cypp.link"];
 
@@ -24,10 +26,29 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 app.use("/user", router);
 User.sync({ force: true });
 Comment.sync({ force: true });
-app.listen("8000", () => {
-  console.log(`
+if (process.env.NODE_ENV == "production") {
+  try {
+    const option = {
+      ca: fs.readFileSync("/etc/letsencrypt/archive/cypp.link/fullchain1.pem"),
+      key: fs.readFileSync("/etc/letsencrypt/archive/cypp.link/privkey1..pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/archive/cypp.link/cert1.pem"),
+    };
+    https.createServer(option, app).listen(8000, () => {
+      console.log(`
+      ####################HTTPS####################
+          🛡️ Server listening on port: 8000 🛡️
+      #############################################    
+      `);
+    });
+  } catch (error) {
+    console.log("https 서버가 열리지 않았습니다.");
+    console.log(error);
+  }
+} else
+  app.listen("8000", () => {
+    console.log(`
       #############################################
           🛡️ Server listening on port: 8000 🛡️
       #############################################    
       `);
-});
+  });
